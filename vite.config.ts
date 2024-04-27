c3972814ade7873fec183b4f7f0b3908e5edaaaf
LePichu <ishat.prsnl65844@gmail.com>
2024-04-27 11:57:57 +0530

    feat: setup fmt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import { resolve } from 'node:path'
+import { resolve } from "node:path"
+import { defineConfig } from "vite"
+import solid from "vite-plugin-solid"
 export default defineConfig({
-  plugins: [solid()],
-  build: {
-          lib: {
-        entry: resolve(__dirname, 'lib/main.ts'),
-          formats: ['es']
-     }
-}
+	plugins: [solid()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "lib/main.ts"),
+			formats: ["es"],
+		},
+	},
 })
